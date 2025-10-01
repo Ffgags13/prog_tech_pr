fefb82a2e602aa2a97aee98457ea8917ae2584d7
Nick <146239966+Ffgags13@users.noreply.github.com>
2025-10-02 00:13:21 +0300

    "6_step"

--- simple_code.c
+++ simple_code.c
@@ -4,6 +4,6 @@ float b;
 int main(){
 	printf('Enter 2 numbers')
 	scanf("%f  %f",&a, &b);
-	printf("%f  %c  %f  %c  %f\n",a,'*', b,'=',  a*b);
+	printf("%f  %c  %f  %c  %f\n",a,'*/', b,'=',  a/b);
 }
 
